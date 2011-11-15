fd9c114fa02ad91204ce8c60aaab016d83c8ebf9
Parantapa Bhattacharya <parantapa@gmail.com>
2011-11-15 13:47:30 +0530

    Initial commit

---
+++ README.rst
@@ -0,0 +1,10 @@
+Pomodoro plugin for Xfce4 panel
+===============================
+
+This bash script uses xfce4-genmon-plugin and libnotify to create a simple
+Pomodoro timer for Xfce4 panel.
+
+The icons are stolen form http://www.flickr.com/photos/bcolbow/3842129453/
+
+Happy Pomodoro !!
+
---
+++ pomodoro.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# This is a simple script for pomodoro timer.
+# This is intended to be used with xfce4-genmon-plugin.
+
+size=24		# Icon size in pixels
+ptime=25	# Time for the pomodoro cycle in minutes
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+state="$DIR/state"
+lock="$DIR/lock"
+
+cycle=$(( ptime * 60 ))
+summary="Pomodoro"
+startmsg="Pomodoro started, you have 25 minutes left"
+endmsg="Pomodoro ended, stop the work and take short break"
+killmsg="Pomodoro stopped, restart when you are ready"
+
+xnotify="notify-send -t 5000 -u critical -i \"$icon\" \"$summary\""
+
+( flock -x 200
+
+stime=$( cat "$state" 2> /dev/null )
+ctime=$( date +%s )
+
+if [ -z "$stime" ] ; then
+	stime=0
+fi
+
+rtime=$(( cycle + stime - ctime))
+
+if [ "$1" == "-n" ] ; then
+	if [ $stime -eq 0 ] ; then
+		$xnotify "$startmsg"
+		echo $ctime > "$state"
+
+	else
+		$xnotify "$killmsg"
+		echo "" > "$state"
+
+	fi
+else
+	echo "<click>$DIR/pomodoro.sh -n</click>"
+
+	if [ $stime -eq 0 ] ; then
+		echo "<img>$DIR/icons/stopped$size.png</img>"
+		echo "<tool>No Pomodoro Running</tool>"
+
+	elif [ $rtime -lt 0 ] ; then
+		$xnotify "$endmsg"
+		echo "" > "$state"
+		echo "<img>$DIR/icons/stopped$size.png</img>"
+		echo "<tool>No Pomodoro Running</tool>"
+
+	else
+		echo "<img>$DIR/icons/running$size.png</img>"
+		echo "<tool>You have $(( rtime / 60 )):$(( rtime % 60 )) min left</tool>"
+	fi
+fi
+
+) 200> "$lock"
+
